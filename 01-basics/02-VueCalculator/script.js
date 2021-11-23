c3f488d417c4d0d4564c4d044863b74b698a8d4c
Maziya_SI <maziya_si@medinfocenter.ru>
2021-11-23 09:28:37 +0500

    VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,26 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const app = createApp({
+    data() {
+        const mathSigns = [
+            { sign: 'sum', calcFunc: (v1, v2) => v1 + v2, icon: '➕' },
+            { sign: 'subtract', calcFunc: (v1, v2) => v1 - v2, icon: '➖' },
+            { sign: 'multiply', calcFunc: (v1, v2) => v1 * v2, icon: '✖' },
+            { sign: 'divide', calcFunc: (v1, v2) => v1 / v2, icon: '➗' },
+          ];
+
+          return {
+            firstValue: 0,
+            secondValue: 0,
+            mathSigns: mathSigns,
+            selectedMathSign: mathSigns[0],
+          };
+    },
+    computed: {
+        result() {
+          const selectedCalcFunc = this.selectedMathSign.calcFunc;
+          return selectedCalcFunc(this.firstValue, this.secondValue);
+        },
+      }
+}).mount('#app');
